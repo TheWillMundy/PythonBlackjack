4ef41a979f05c6309a304265b70b6c9fc6e6c14f
Will Mundy <kday5and75@gmail.com>
2019-10-06 22:48:16 -0500

    README changes and updates to main.py to enhance modularization

--- README.md
+++ README.md
@@ -15,6 +15,21 @@ The `main.py` file contains the main game logic, handling the main flow of the g
 
 The `card.py` file contains a `CardClass` which the `deck.py` file uses to instantiate new cards in the deck object's `create_deck` method. Each card has a suit, numeric value, and "name", where the name allows for face cards and aces to be identified. Additionally, the card class has two helper methods to handle the ace edge case (one to check whether the card is an ace, the other to toggle the value between 1 and 11 depending on the user's choice). All cards that are aces have an initial value of 1. Cards also have a `to_string` method which prints their name and suit. 
 
-The `deck.py` file contains a `DeckClass` which is instantiated to create a new deck object. Its method `create_deck` creates a full, ordered deck of 52 cards with all 4 suits represented. 
+The `deck.py` file contains a `DeckClass` which is instantiated to create a new deck object. Its method `create_deck` creates a full, ordered deck of 52 cards with all 4 suits represented. The deck is in an array structure, with each element being a single card element, as this structure could be easily shuffled using the random module.
 
-The `player.py` file contains a `PlayerClass` which can be instantiated to create a new player. This player object stores its cards in an array since the order of the cards doesn't
+The `player.py` file contains a `PlayerClass` which can be instantiated to create a new player. This player object stores its cards in an array since the order of the cards doesn't matter. Each card is its own object since for aces the value might change, and it is possible for a player to have more than one ace, so those two cards are not interchangeable. The player class also has some helper methods which allow for the main program driver to check whether they are busted, filter their card array to only aces, or reset their values so that the player can play with a fresh deal. 
+
+
+# Choice of tooling
+## Language: Python
+I chose to use Python since it is very easy to create a terminal application with it, and the ability to hide previous text (through the terminal clear screen command) allows for each player to only view their own cards. Additionally, Python's ability to create classes allowed for a high degree of modularization between the different structures involved. Similarly, the ability to easily import modules allowed for the deck class to use the card class, as well as the main driver to use the card, deck, and player classes. Additionally, since we used Python we employed snake case for almost all of the program except for classes.
+
+## Additional Libraries
+### random
+This was used in order to shuffle the deck, using its shuffle method.
+
+### os & platform
+`platform` was used to check the system platform, and then depending on the system (Windows or Linux/OSX) the `clear_screen` method could use the correct system call to clear the screen using the `os` module.
+
+### Testing library
+`unittest` was used so that 
--- main.py
+++ main.py
@@ -11,14 +11,11 @@ def clear_screen():
     else:
         _=os.system('clear')
 
-def main():
+def input_players():
     players = []
-
-    print "Welcome to BlackJack!"
-    # Set number of players
     flag = False
+
     while (True):
-        # Check that they input an integer
         try:
             player_ct = input("How many players would like to play (1-4): ")
             player_ct = int(player_ct)
@@ -29,104 +26,120 @@ def main():
             break
         except:
             if (flag):
-                return
+                return False
             print "Please enter a valid integer, or any character to exit."
-            flag = True
+            flag = True 
+    return players
+
+def initial_deal(players, shuffled_deck):
+    for player in players:
+        # Reset players
+        player.reset()
+        player_no = player.get_player_no()
+        print("Player %s, you have been dealt: \n" % player_no)
+        for _ in range(2):
+            dealt_card = shuffled_deck.pop()
+            print(dealt_card.to_string())
+            player.add_card(dealt_card)
+        # Post initial dealing
+        total_value = player.get_total_value()
+        print("The total value of your cards is: %s" % total_value)
+        if (player_no < len(players)):
+            raw_input("Type anything once you are ready to continue to the next player.")
+            clear_screen()
+        else:
+            raw_input("Type anything once you are ready for the dealer to deal themselves in.")
+            clear_screen()
+
+def hit_or_stay(players, shuffled_deck, dealer):
+    for player in players:
+        flag = False
+        player_no = player.get_player_no()
+        print("The dealer's face up card is: %s" % dealer.get_last_card().to_string())
+        while (True):
+            aces = player.filter_aces()
+            if (len(aces) > 0):
+                print("It appears you have %d aces. Below, select whether to adjust their value or not." % len(aces))
+                for ace in aces:
+                    update = raw_input("If you'd like to switch the value of this ace from %d then type 'yes', otherwise type 'no'. \n" % ace.get_value())
+                    if (update == "yes"):
+                        ace.switch_ace_value()
+                    else:
+                        print("Ok, nothing will be done to this ace. \n")
+            try:
+                print("Player %s, your current total hand value is: %s" % (player_no, player.get_total_value()))
+                option = input("Player %s, type 1 to hit or 2 to stand: " % player_no)
+                option = int(option)
+                if (option == 2):
+                    break
+                else:
+                    dealt_card = shuffled_deck.pop()
+                    print(dealt_card.to_string())
+                    player.add_card(dealt_card)
+                    total_value = player.get_total_value()
+                    print("The total value of your cards is now: %s" % total_value)
+                    if (player.check_busted()):
+                        raw_input("You've busted! Type anything to continue.")
+                        clear_screen()
+                        break
+            except:
+                if (flag):
+                    return
+                print("Please enter 1 to hit, 2 to stay, or any character to exit")
+                flag = True
+        clear_screen()
     
-    # First create & shuffle deck for the game
-    deck_cls = deck.DeckClass()
-    shuffled_deck = deck_cls.create_deck()
+def end_game(players, shuffled_deck, dealer):
+    print("The dealer's facedown card is: %s" % dealer.get_cards()[0].to_string())
+    players_in = filter(lambda player : player.check_busted() == False, players)
+    if (len(players_in) > 0 and dealer.get_total_value() < 17):
+        while (dealer.get_total_value() < 17):
+            dealt_card = shuffled_deck.pop()
+            print("The dealer has drawn another card: %s \n" % dealt_card.to_string())
+            dealer.add_card(dealt_card)
+    if (dealer.check_busted()):
+        print("The dealer has busted with a final value of: %s" % dealer.get_total_value())
+    else:
+        print("The dealer hand's final value is: %s" % dealer.get_total_value())
+    
+    if (len(players_in) < 1):
+        print("No players are still in, so the House has won.")
+    else:
+        for player in players_in:
+            player_no = player.get_player_no()
+            if (dealer.check_busted() or player.get_total_value() > dealer.get_total_value()):
+                print("Player %s has beaten the dealer!" % player_no)
+            else:
+                print("Unfortunately Player %s has lost to the dealer." % player_no)
 
-    random.shuffle(shuffled_deck)
+def main():
+    players = []
 
-    # Start game
+    print "Welcome to BlackJack!"
     while (True):
+        players = input_players()
+    
+        # First create & shuffle deck for the game
+        deck_cls = deck.DeckClass()
+        shuffled_deck = deck_cls.create_deck()
+
+        random.shuffle(shuffled_deck)
+
+        # Game starts
         # Phase 1: Deal
         print("The dealing will now begin.\n")
-        for player in players:
-            # Reset players
-            player.reset()
-            player_no = player.get_player_no()
-            print("Player %s, you have been dealt: \n" % player_no)
-            for _ in range(2):
-                dealt_card = shuffled_deck.pop()
-                print(dealt_card.to_string())
-                player.add_card(dealt_card)
-            # Post initial dealing
-            total_value = player.get_total_value()
-            print("The total value of your cards is: %s" % total_value)
-            if (player_no < len(players)):
-                raw_input("Type anything once you are ready to continue to the next player.")
-                clear_screen()
-            else:
-                raw_input("Type anything once you are ready for the dealer to deal themselves in.")
-                clear_screen()
+        initial_deal(players, shuffled_deck)
+
         # Dealer is dealt
         dealer = PlayerClass("Dealer")
         for _ in range(2):
             dealt_card = shuffled_deck.pop()
             dealer.add_card(dealt_card)
 
-        # Loop on hit or not
-        for player in players:
-            flag = False
-            player_no = player.get_player_no()
-            print("The dealer's face up card is: %s" % dealer.get_last_card().to_string())
-            while (True):
-                aces = player.filter_aces()
-                if (len(aces) > 0):
-                    print("It appears you have %d aces. Below, select whether to adjust their value or not." % len(aces))
-                    for ace in aces:
-                        update = raw_input("If you'd like to switch the value of this ace from %d then type 'yes', otherwise type 'no'. \n" % ace.get_value())
-                        if (update == "yes"):
-                            ace.switch_ace_value()
-                        else:
-                            print("Ok, nothing will be done to this ace. \n")
-                try:
-                    print("Player %s, your current total hand value is: %s" % (player_no, player.get_total_value()))
-                    option = input("Player %s, type 1 to hit or 2 to stand: " % player_no)
-                    option = int(option)
-                    if (option == 2):
-                        break
-                    else:
-                        dealt_card = shuffled_deck.pop()
-                        print(dealt_card.to_string())
-                        player.add_card(dealt_card)
-                        total_value = player.get_total_value()
-                        print("The total value of your cards is now: %s" % total_value)
-                        if (player.check_busted()):
-                            raw_input("You've busted! Type anything to continue.")
-                            clear_screen()
-                            break
-                except:
-                    if (flag):
-                        return
-                    print("Please enter 1 to hit, 2 to stay, or any character to exit")
-                    flag = True
-            clear_screen()
+        hit_or_stay(players, shuffled_deck, dealer)
         
-        # Now the dealer's turn
-        print("The dealer's facedown card is: %s" % dealer.get_cards()[0].to_string())
-        players_in = filter(lambda player : player.check_busted() == False, players)
-        if (len(players_in) > 0 and dealer.get_total_value() < 17):
-            while (dealer.get_total_value() < 17):
-                dealt_card = shuffled_deck.pop()
-                print("The dealer has drawn another card: %s \n" % dealt_card.to_string())
-                dealer.add_card(dealt_card)
-        if (dealer.check_busted()):
-            print("The dealer has busted with a final value of: %s" % dealer.get_total_value())
-        else:
-            print("The dealer hand's final value is: %s" % dealer.get_total_value())
-        
-        if (len(players_in) < 1):
-            print("No players are still in, so the House has won.")
-        else:
-            for player in players_in:
-                player_no = player.get_player_no()
-                if (dealer.check_busted() or player.get_total_value() > dealer.get_total_value()):
-                    print("Player %s has beaten the dealer!" % player_no)
-                else:
-                    print("Unfortunately Player %s has lost to the dealer." % player_no)
+        end_game(players, shuffled_deck, dealer)
+
         play_again = raw_input("If you'd like to play again, type 'yes': ")
         if (play_again != "yes"):
             return
